bcb71e6d932c0cb6481a2ed66d7722caf4664e20
Jaeyun Lee <46063222+EdwardLeeData@users.noreply.github.com>
2022-12-28 16:07:59 -0800

    Add files via upload

---
+++ dotNet/Models/StripeCustomer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain
+{
+    public class StripeSubscribedCustomer
+    {
+        public string SubscriptionId { get; set; }
+        public string CustomerId { get; set; }
+    }
+}
---
+++ dotNet/Models/StripeInvoicePeriod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain
+{
+    public class StripeInvoicePeriod
+    {
+        public DateTime InvoiceStart { get; set; }
+        public DateTime InvoiceEnd { get; set; }
+    }
+}
